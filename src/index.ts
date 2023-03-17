c0b59bccde2ca252573c7c0a56e8650af43e4ce9
Yixin <yixin88@hotmail.co.uk>
2023-03-17 00:02:42 +0000

    added enum example and syntax

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,10 @@ let numbers: number[] = []; //array
 
 let user: [number, string] = [1, 'Yixin']; //tuple
 user.push(2); //Typescript will not check the push method !!could cause error
+
+
+//enum (PascalCase)
+const enum Size { Small = 1, Medium, Large}; 
+//^^enum will start with 0 by default, but can be set manually like 1^^
+//^^the rest will follow order number, if its a string then need to manully set all
+let mySize: Size = Size.Medium;
