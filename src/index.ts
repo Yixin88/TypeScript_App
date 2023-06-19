12b4708aabefbceee386268ef0e55421f57aaf40
Yixin <yixin88@hotmail.co.uk>
2023-06-19 15:13:10 +0100

    fixed typo

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ user.push(2); //Typescript will not check the push method !!could cause error
 //enum (PascalCase)
 const enum Size { Small = 1, Medium, Large}; 
 //^^enum will start with 0 by default, but can be set manually like 1^^
-//^^the rest will follow order number, if its a string then need to manully set all
+//^^the rest will follow order number, if its a string then need to manually set all
 let mySize: Size = Size.Medium;
 
 
@@ -40,14 +40,14 @@ function calculateTax(income: number, taxYear=2022): number { //parameter can se
 
 calculateTax(10000, 2022, 1); // If there are 3 parameter is provided, tsc will warn you
 calculateTax(10000); //If theres only 1 parameter is provided, we can put ? in the function parameter e.g. taxYear?:number
-//this tells tsc that not nessceary that all parameter need to be provided, but can cause problem inside the function
+//this tells tsc that not necessary that all parameter need to be provided, but can cause problem inside the function
 
 
 //------------------------------------------------------------------------//
 
 //Objects
 let employee: {
-    readonly id: number, //adding readonly means that this property can not be modnify after declaring
+    readonly id: number, //adding readonly means that this property can not be modify after declaring
     name: string,
     fax?: number, //adding question mark mean that this is optional and can be provided or not
     retire: (date: Date) => void //this is how to type for function in object, void means return nothing
@@ -58,7 +58,7 @@ let employee: {
         console.log(Date)
     }
 }
-//The code can be messy and will need to be reused everytime when setting a object
+//The code can be messy and will need to be reused every time when setting a object
 //We can use Type Aliases to have a set of types instead, refer to line 1
 //So now if we can use that template to set our type, using employee 2 as a new example
 let employee2: Employee = {
@@ -73,7 +73,7 @@ let employee2: Employee = {
 //------------------------------------------------------------------------//
 
 //Union Type
-function kgToLbs(weight: number | string): number { //putting | in for it to have differnt type, so can be a number or string
+function kgToLbs(weight: number | string): number { //putting | in for it to have different type, so can be a number or string
     //narrowing, getting the recommendation method for each type
     if (typeof weight === 'number') //now this will only recommend method for number
         return weight * 2.2;
@@ -84,7 +84,7 @@ function kgToLbs(weight: number | string): number { //putting | in for it to hav
 
 //------------------------------------------------------------------------//
 
-//Intersection Type, combining multiple tempalte together
+//Intersection Type, combining multiple template together
 type Draggable = { //template 1
     drag: () => void
 };
@@ -130,6 +130,6 @@ console.log(customer?.birthday); //by adding ? we can say that this line of code
 //Optional element access operator, for array, e.g. if there an array that is null or undefined, the following line of code can be not run by adding ?.
 // customer?.[0] <-- only run if there is something in the array
 
-//Optionl call
+//Optional call
 let log: any = null;
 log?.('a'); //this means this line of code will only execute if log is referencing to an actual function
