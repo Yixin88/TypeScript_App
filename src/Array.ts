f79b48bbad58ce18af2e03477398b363ea5d3d3b
Yixin <yixin88@hotmail.co.uk>
2023-06-26 22:58:35 +0100

    added additional array examples

--- src/Array.ts
+++ src/Array.ts
@@ -6,6 +6,7 @@ superHero.push('three')
 const superHero2: number[] = [1, 2, 3, 4] 
 superHero2.push(5)
 
-//both string or number array
-const superHero3: (string|number)[] = [1, '2', 3] 
-superHero.push('four')
+//both string or number array, two different ways
+const superHero3: (string|number)[] = [1, '2', 3]  //1st way
+const superHero4: Array<string|number> = [1, '2', 3]  //2nd way
+superHero3.push('four')
