65a08dc1fb647193a4b0f82147cf382cc5d94145
Yixin <yixin88@hotmail.co.uk>
2023-06-28 16:11:12 +0100

    added some interface examples

---
+++ src/interface.ts
@@ -0,0 +1,26 @@
+interface Users {
+  email: string
+  userId: number
+}
+
+//interface are very similar to type, but it allows extending
+interface Animal {
+  name: string
+}
+
+interface Bear extends Animal { //now we can extend it and also saying Bear interface will have the same property as Animal as well as what is on top
+  honey: boolean
+}
+
+const bear: Bear = {name: 'Bob', honey: true} 
+
+//Compare to type
+type Animal2 = {
+  name: string
+}
+
+type Bear2 = Animal2 & {
+  honey: boolean
+}
+
+const bear2: Bear2 = {name: 'Bob', honey: true} 
