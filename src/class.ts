e12fe96c14b2c6e6ec03a2852597fe4961d9291a
Yixin <yixin88@hotmail.co.uk>
2023-06-28 17:25:08 +0100

    added classes examples

---
+++ src/class.ts
@@ -0,0 +1,12 @@
+class Car {
+  carType: string //these ts code declares type of name and carType on line 6 and 7, which is different than the constructor arguments
+  name: string
+  doors: number = 5; //We can also give some default value 
+
+  constructor(name: string, carType: string) {
+    this.name = name;
+    this.carType = carType;
+  }
+}
+
+const toyota = new Car('Yaris', 'Hatchback')
